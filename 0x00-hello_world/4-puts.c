affb9995f9d2217990e2c8a8664a1c71db1157de
fsarpong <opokusarpongpapa@gmail.com>
2022-09-08 13:45:28 -0700

    bash and c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program printing using puts function
+ **/
+
+int main()
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
